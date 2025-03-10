0579c1d25a1d1e3be2ff18f9d4242a3b3d344d41
9wajda <jdoe@example.com>
2025-03-10 01:33:42 +0100

    Disable database connection temporarily

---
+++ Movies.Api/Program.cs
@@ -0,0 +1,38 @@
+using Movies.Application.Repositories;
+using Movies.Application;
+using Movies.Application.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddDatabase(config["Database:ConnectionString"]!);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+var bdInitializer = app.Services.GetRequiredService<DbInitializer>();
+await bdInitializer.InitializeAsync();
+
+app.Run();
