1fd15724779a6c596c9f5f65014edc0f248601ee
9wajda <jdoe@example.com>
2025-03-12 03:15:38 +0100

    Finish Rating Movie

--- Movies.Application/Repositories/IRatingRepository.cs
+++ Movies.Application/Repositories/IRatingRepository.cs
@@ -2,6 +2,7 @@
 
 public interface IRatingRepository
 {
+    Task<bool> RateMovieAsync(Guid movieId, int rating, Guid userId, CancellationToken token = default);
     Task<float?> GetRatingAsync(Guid movieid, CancellationToken token = default);
     Task<(float? Rating, int? UserRating)> GetRatingAsync(Guid movieid, Guid userId, CancellationToken token = default);
 }
---
+++ Movies.Application/Services/RatingService.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation.Results;
+using Movies.Application.Repositories;
+using ValidationException = FluentValidation.ValidationException;
+
+namespace Movies.Application.Services;
+
+public class RatingService: IRatingService
+{
+    private readonly IRatingRepository _ratingRepository;
+    private readonly IMovieRepository _movieRepository;
+
+    public RatingService(IRatingRepository ratingRepository, IMovieRepository movieRepository)
+    {
+        _ratingRepository = ratingRepository;
+        _movieRepository = movieRepository;
+    }
+
+    public async Task<bool> RateMovieAsync(Guid movieId, int rating, Guid userId, CancellationToken token = default)
+    {
+        if (rating is <= 0 or > 10)
+        {
+            throw new ValidationException(new[]
+            {
+                new ValidationFailure
+                {
+                    PropertyName = "Rating",
+                    ErrorMessage = "Rating must be between 0 and 10"
+                }
+            });
+        }
+        var movieExists = await _movieRepository.ExistByIdAsync(movieId, token);
+        if (!movieExists)
+        {
+            return false;
+        }
+        
+        return await _ratingRepository.RateMovieAsync(movieId, rating, userId, token);
+    }
+}
